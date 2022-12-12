3dc70151f320457df4ac848113ea42e12ac4216a
Brandddd <b.palacio1@utp.edu.co>
2022-12-11 23:06:54 -0500

    Creación de AdminBlade para ejemplo de yield y section

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">  {{--- token de laravel para verificar usuarios---}}
+
+    <title>{{ config('app.name', 'Laravel') }}</title>   {{--- Nombre de la aplicación ---}}
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div id="app">
+		<h1>
+			Admin
+		</h1>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>

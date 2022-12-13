22044c507534f6f83a90270af3782758b97564e7
Brandddd <b.palacio1@utp.edu.co>
2022-12-13 17:48:06 -0500

    Tabla responsive.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,47 +6,49 @@
                 <a href="{{ route('user.create') }}" class="btn btn-primary">Crear usuario</a>
             </div>
             <div class="card-body">
-                <table class="table my-4 mx-3 justify-content-center">
-                    <thead>
-                        <tr>
-                            <th scope="col">
-                                <h4><strong>Identificación</strong></h4>
-                            </th>
-                            <th scope="col">
-                                <h4><strong>Nombre</strong></h4>
-                            </th>
-                            <th scope="col">
-                                <h4><strong>Apellido</strong></h4>
-                            </th>
-                            <th scope="col">
-                                <h4><strong>Correo Electrónico</strong></h4>
-                            </th>
-                            <th scope="col">
-                                <h4><strong>Acciones</strong></h4>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($users as $user)
+                <section class="table-responsive">
+                    <table class="table my-4 mx-3 justify-content-center">
+                        <thead>
                             <tr>
-                                <th>{{ $user->number_id }}</th>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->last_name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td class="d-flex">
-                                    <a href="{{ route('user.edit', ['user' => $user->id]) }}"
-                                        class="btn btn-warning btn-sm me-1">Editar</a>
-
-                                    <form action="{{ route('user.delete', ['user' => $user->id]) }}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
-                                    </form>
-                                </td>
+                                <th scope="col">
+                                    <h4><strong>Identificación</strong></h4>
+                                </th>
+                                <th scope="col">
+                                    <h4><strong>Nombre</strong></h4>
+                                </th>
+                                <th scope="col">
+                                    <h4><strong>Apellido</strong></h4>
+                                </th>
+                                <th scope="col">
+                                    <h4><strong>Correo Electrónico</strong></h4>
+                                </th>
+                                <th scope="col">
+                                    <h4><strong>Acciones</strong></h4>
+                                </th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <th>{{ $user->number_id }}</th>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->last_name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td class="d-flex">
+                                        <a href="{{ route('user.edit', ['user' => $user->id]) }}"
+                                            class="btn btn-warning btn-sm me-1">Editar</a>
+
+                                        <form action="{{ route('user.delete', ['user' => $user->id]) }}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </section>
             </div>
         </div>
     </section>

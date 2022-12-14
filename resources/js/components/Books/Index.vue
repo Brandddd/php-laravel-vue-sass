3e083e686a40e981b29e96d82b9cd1b73fa0f776
Brandddd <b.palacio1@utp.edu.co>
2022-12-13 21:50:05 -0500

    Creación de los componentes para Books usando Vue, además de su implementación.

--- resources/js/components/Books/Index.vue
+++ resources/js/components/Books/Index.vue
@@ -1,18 +1,62 @@
 <template>
 	<div class="card mx-5 my-5">
-
 		<div class="card-header d-flex justify-content-between">
 			<h2><strong>Libros</strong></h2>
 			<a class="btn btn-primary">Crear libro</a>
 		</div>
 
 		<div class="card-body">
-			<h1>Hola desde Vue</h1>
+			<section class="table-responsive" v-if="load">
+				<!-- Se le envían los datos de books por props a la tabla -->
+				<table-component :books_data="books" />
+			</section>
+			<!-- load -->
+			<section class="d-flex justify-content-center my-3" v-else>
+				<div class="spinner-border text-primary" role="status">
+					<span class="visually-hidden">Loading...</span>
+				</div>
+			</section>
 		</div>
-
 	</div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+import TableComponent from './Table.vue'
+
+export default {
+	props: [],
+	components: {
+		TableComponent
+	},
+	data() {
+		return {
+			books: [],
+			load: false
+		}
+	},
+	created() {
+		this.index()
+	},
+	mounted() {},
+
+	methods: {
+		async index() {
+			// Obtencion del objeto books desde getBooks()
+			await this.getBooks()
+		},
+		async getBooks() {
+			try {
+				// Consumiento la api desde la api.php con axios para obtener el objeto books y mandandola como data
+				const { data } = await axios.get('/api/Books/GetAllBooks')
+				this.books = data.books
+				this.load = true
+				// console.log(books)
+			} catch (error) {
+				console.error(error)
+			}
+		}
+	}
+}
 </script>
+

decb27b703a4f97a1b1c695307488ae840856073
Brandddd <b.palacio1@utp.edu.co>
2022-12-14 17:07:26 -0500

    Importando v-select en app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import './bootstrap'
 import { createApp } from 'vue'
 
+import vSelect from 'vue-select'
+
 // -------------------------- Componentes -------------------------
 import ExampleComponent from './components/ExampleComponent.vue' // Forma de importar componentes
 import BooksList from './components/Books/Index.vue'
@@ -11,6 +13,8 @@ const app = createApp({
 		BooksList
 	}
 })
+
+app.component('v-select', vSelect)
 
 // Para usar los componentes con un div, deben tener el id='app'
 app.mount('#app') // Se monta la aplicacion en un id que se llama app, se llama en layouts/app.blade.php
